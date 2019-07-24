b90fbe88069eda39d16265183c8f03fef2fbbe23
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-24 15:28:09 +0100

    Add JWT methods

---
+++ jwt.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const UsersDB = require('./users/users-model');
+
+const secret = process.env.JWT_SECRET || "34hg5vjkbhmdfiuv882j345hbb_sd7fg8%$sdjfgn$2CVfsdf&sj*lsdgjmn3@";
+
+function generateToken(user) {
+    const payload = {
+        sub: user.id,
+        username: user.username,
+    }
+
+    const opetions = {
+        expiresIn: '20d',
+    }
+
+    return jwt.sign(payload, secret, options);
+}
+
+async function restricted(req, res, next) {
+    const token = req.header.authorization;
+    if(token) {
+        try {
+            const decoded = jwt.verify(token, secret);
+            if (!decoded) throw new Error('Not Valid');
+            req.user = await UsersDB.findById(decoded.sub);
+            next();
+            return;
+        } catch (error) {
+            res.status(401).json({ error: "Not allowed here!" });
+        }
+    } else {
+        res.status(401).json({ error: "Not allowed here!" });
+    }
+}
