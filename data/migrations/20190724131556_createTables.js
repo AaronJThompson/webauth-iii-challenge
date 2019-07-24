90798e1787e514371efb558c2d12fcb412e8ad18
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-24 13:23:25 +0100

    Add users table

---
+++ data/migrations/20190724131556_createTables.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+        tbl.text('username', 128).notNullable().unique();
+        tbl.text('password', 256).notNullable();
+        tbl.text('department', 128);
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExisits('users');
+};
